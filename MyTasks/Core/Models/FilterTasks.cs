2318f9c16e7672513b700df8ad3721e3bf9681b4
m-sobas <m.sobas@fejm.pl>
2023-01-26 21:18:50 +0100

    W-10 L-6

---
+++ MyTasks/Core/Models/FilterTasks.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTasks.Core.Models
+{
+    public class FilterTasks
+    {
+        public string Title { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [Display(Name="Tylko zrealizowane")]
+        public bool IsExecuted { get; set; }
+    }
+}
